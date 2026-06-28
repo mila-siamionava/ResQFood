7de813a859c6e7702e51f1b21eb1c7650be9eb7a
Ahmad <ahmad.pakmehr@gmail.com>
2026-06-29 00:19:04 +0200

    Hard coded center leaf logo with image logo

--- components/FrontpageBanner/FrontpageBanner.jsx
+++ components/FrontpageBanner/FrontpageBanner.jsx
@@ -1,5 +1,7 @@
 import { Logo } from "@/components/ui/Logo/Logo"; 
 import styles from "./FrontpageBanner.module.css";
+import Image from "next/image";
+import leafIcon from "./assets/leaf.png";
 
 export default function FrontpageBanner() {
 	return (
@@ -8,9 +10,7 @@ export default function FrontpageBanner() {
                     <Logo className={styles.logo} />
 				</div>
 
-                <p className={styles.centerLeaf}>Leaf</p>
-
-				{/* <Image className={styles.centerLeaf} src={leafIcon} alt="" aria-hidden="true" /> */}
+                <Image className={styles.centerLeaf} src={leafIcon} alt="" aria-hidden="true" />
 
 				<div className={`${styles.rectangle} ${styles.rightRectangle}`}>
                     <p className={styles.sallingText}>sa<span className={styles.sallingUnderlined}>ll</span>ing</p>
