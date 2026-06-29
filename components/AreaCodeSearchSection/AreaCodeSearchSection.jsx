e4136dd3aae074e90b6ae74c50bb43db93e7f21c
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-06-29 16:35:24 +0200

    fix formatting

--- components/AreaCodeSearchSection/AreaCodeSearchSection.jsx
+++ components/AreaCodeSearchSection/AreaCodeSearchSection.jsx
@@ -3,15 +3,15 @@ import { useRouter } from "next/navigation";
 import AreaCodeSearch from "../AreaCodeSearch/AreaCodeSearch";
 
 export default function AreaCodeSearchSection() {
- const router = useRouter();
+  const router = useRouter();
 
   const handleAreaSearch = (validAreaCode) => {
-     router.push(`/stores?zip=${validAreaCode}`);
-    };
-    
+    router.push(`/stores?zip=${validAreaCode}`);
+  };
+
   return (
     <section>
       <AreaCodeSearch onAreaCodeSearch={handleAreaSearch} />
-         </section>
+    </section>
   );
 }
