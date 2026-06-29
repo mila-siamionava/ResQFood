401798fdd4fc80d5210dda746cbd18eba0aedf98
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-06-29 15:12:23 +0200

    refactor: add item props

--- components/ui/InfoList/InfoList.jsx
+++ components/ui/InfoList/InfoList.jsx
@@ -1,24 +1,26 @@
 import { Logo } from "@/components/ui/Logo/Logo";
 import styles from "./InfoList.module.css";
-export default function InfoList() {
+
+const infoItems = [
+  {
+    text: "Salling Group and ResQFood have partnered to reduce food waste.",
+  },
+  {
+    text: "Every day, over 10,000 discounted food products are available in local stores.",
+  },
+  {
+    text: "Find offers near you and help rescue perfectly good food before it goes to waste.",
+  },
+];
+export default function InfoList({ items = infoItems }) {
   return (
     <div className={styles.list}>
-      <p>
-        <Logo markOnly className={styles.logo} />
-        Salling Group and ResQFood have partnered to reduce food waste.
-      </p>
-
-      <p>
-        <Logo markOnly className={styles.logo} />
-        Every day, over 10,000 discounted food products are available in local
-        stores.
-      </p>
-
-      <p>
-        <Logo markOnly className={styles.logo} />
-        Find offers near you and help rescue perfectly good food before it goes
-        to waste.
-      </p>
+      {items.map((item) => (
+        <p key={item.text}>
+          <Logo markOnly size="sm" className={styles.logo} />
+          {item.text}
+        </p>
+      ))}
     </div>
   );
 }
