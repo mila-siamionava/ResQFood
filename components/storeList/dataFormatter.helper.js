5761df36770d33e345fa0001e037a3391686d8d9
MKanunnikau <matea.ekv@gmail.com>
2026-07-04 00:42:10 +0200

    refactor dataFormatter file to extract util functions and to fix functions

--- components/storeList/dataFormatter.helper.js
+++ components/storeList/dataFormatter.helper.js
@@ -1,55 +1,25 @@
-import formatTime from "../../utils/formatTime";
-import closingTimeCountdown from "../../utils/closingTime";
-
-const storeName = {
-  netto: "Netto",
-  foetex: "føtex",
-  bilka: "Bilka",
-};
-
-const getStoreName = (key) => storeName[key.toLowerCase()] || key;
-
-const formatDistance = (distance) => {
-  if (distance == null) return null;
-  return distance > 1
-    ? `${Number(distance).toPrecision(1)}km`
-    : `${Math.round(distance * 1000)}m`;
-};
-
-const getWorkingHours = (isClosed, openingTime, closingTime) => {
-  const hoursUntilClosing = closingTimeCountdown(closingTime);
-  if (isClosed || hoursUntilClosing <= 0)
-    return {
-      status: "closed",
-      openHours: `Opens at ${formatTime(openingTime)}`,
-    };
-
-  if (hoursUntilClosing > 1)
-    return {
-      status: "open",
-      openHours: `Closing at ${formatTime(closingTime)}`,
-    };
-
-  return {
-    status: "closingSoon",
-    openHours: `Closing soon at ${formatTime(closingTime)}`,
-  };
-};
+import { getStoreName } from "@/app/brands";
+import formatDistance from "@/utils/formatDistance";
+import {
+  getTodaySchedule,
+  getStatus,
+  getWorkingHours,
+} from "@/utils/formatWorkingStatus";
 
 export default function dataFormatter(store) {
-  console.log("I was here!");
-  console.log(store);
   if (!store) return null;
 
-  const todaySchedule = store.hours?.[0];
+  const todaySchedule = getTodaySchedule(store.hours);
+  const currentStatus = getStatus(todaySchedule?.closed, todaySchedule?.close);
 
   const formattedObj = {
     name: getStoreName(store.brand),
     brand: store.brand,
     address: store.address?.street,
     distance: formatDistance(store?.distance_km),
+    status: currentStatus,
     workingHours: getWorkingHours(
-      todaySchedule?.closed,
+      currentStatus,
       todaySchedule?.open,
       todaySchedule?.close,
     ),
