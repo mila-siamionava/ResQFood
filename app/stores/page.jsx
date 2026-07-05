4164d2cbd54c69ccf416fa4b250ce73d2984fe93
MKanunnikau <matea.ekv@gmail.com>
2026-07-05 13:27:47 +0200

    start on the stores page and the utility styling

--- app/stores/page.jsx
+++ app/stores/page.jsx
@@ -1,4 +1,12 @@
+import StoreList from "@/components/storeList/StoreList";
+import data from "../mockData/foodWaste";
+import {
+  getFoodWasteByGeo,
+  getFoodWasteByZip,
+} from "@/services/foodWasteService";
+
 export default async function Stores({ searchParams }) {
+  let storeData = [];
   const params = await searchParams;
 
   const source = params.source; //tells if it is search or geolocation
@@ -6,10 +14,19 @@ export default async function Stores({ searchParams }) {
   const latitude = params.lat;
   const longitude = params.lng;
 
-  const storeData = 0;
+  if (source === "search" && areaCode) {
+    storeData = await getFoodWasteByZip(areaCode);
+  } else if (source === "geolocation" && latitude && longitude) {
+    storeData = await getFoodWasteByGeo(latitude, longitude);
+  } else {
+    storeData = data;
+  }
+
   return (
     <>
+      <p className="storeList-heading">Showing stores for {areaCode}</p>
       <StoreList data={storeData} />
+      {/* remove this component and put it here */}
     </>
   );
 }
