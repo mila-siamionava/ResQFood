b79d9d35815e75d5d3fcc2ded3433dee584fd5f1
MKanunnikau <matea.ekv@gmail.com>
2026-07-05 13:37:08 +0200

    add headline message

--- app/stores/page.jsx
+++ app/stores/page.jsx
@@ -1,5 +1,4 @@
 import StoreList from "@/components/storeList/StoreList";
-import data from "../mockData/foodWaste";
 import {
   getFoodWasteByGeo,
   getFoodWasteByZip,
@@ -18,15 +17,15 @@ export default async function Stores({ searchParams }) {
     storeData = await getFoodWasteByZip(areaCode);
   } else if (source === "geolocation" && latitude && longitude) {
     storeData = await getFoodWasteByGeo(latitude, longitude);
-  } else {
-    storeData = data;
   }
 
   return (
     <>
-      <p className="storeList-heading">Showing stores for {areaCode}</p>
+      <p className="storeList-heading">
+        Showing stores {areaCode ? `for ${areaCode}` : `near you`}
+      </p>
       <StoreList data={storeData} />
-      {/* remove this component and put it here */}
+      {/* remove this component and put the logic here instead*/}
     </>
   );
 }
